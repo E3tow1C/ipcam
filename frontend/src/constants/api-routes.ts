b8aebad133a7e3665310a89a1d030b25d35dd143
Thepphithak <95979163+E3tow1C@users.noreply.github.com>
2025-02-23 16:42:02 +0700

    enhance fetchAPI type definitions

--- frontend/src/constants/api-routes.ts
+++ frontend/src/constants/api-routes.ts
@@ -1,10 +1,7 @@
 export const BASE_API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const API_ROUTES = {
-  IPCAM: {
-    GET: `${BASE_API_URL}/ipcam`,
-    POST: `${BASE_API_URL}/ipcam`,
-  },
+  IPCAM: `${BASE_API_URL}/upload`,
   UPLOAD: `${BASE_API_URL}/upload`,
   HEALTH_CHECK: `${BASE_API_URL}/`,
 };
