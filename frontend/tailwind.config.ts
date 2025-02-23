72a6720b5bcb92c1f2950e3ef90b64e42eb9a0ce
Pirapat Tarkon <96107349+PirapatTK@users.noreply.github.com>
2025-02-23 16:59:44 +0700

    Add slide-up animation and keyframes to Tailwind configuration

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -14,12 +14,17 @@ export default {
       },
       animation: {
         'slide-in-left': 'slide-in-left .3s ease-in-out',
+        'slide-up': 'slide-up .3s ease-in-out',
       },
       keyframes: {
         'slide-in-left': {
           '0%': { transform: 'translateX(-100%)' },
           '100%': { transform: 'translateX(0)' },
         },
+        'slide-up': {
+          '0%': { transform: 'translateY(20)', opacity: "0", scale: "0.9" },
+          '100%': { transform: 'translateY(0)', opacity: "1", scale: "1" },
+        }
       }
     },
   },
