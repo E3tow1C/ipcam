63444337f6a0c62313c60006f5b9525499307c37
Thepphithak <95979163+E3tow1C@users.noreply.github.com>
2025-03-11 05:44:37 +0700

    Update load testing script to reduce virtual users and target image endpoint

--- deployment/script/load-test.js
+++ deployment/script/load-test.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 5000,           // Virtual Users (concurrent users)
+  vus: 2000,           // Virtual Users (concurrent users)
   duration: '300s',   // Test duration
 };
 
 export default function () {
-  http.get('http://fastapi.localhost:8080');
+  http.get('http://fastapi.localhost:8080/images');
   sleep(0.5);
 }
