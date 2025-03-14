95586afb0798ec2d25859a733e7152b1307364b6
Thepphithak <95979163+E3tow1C@users.noreply.github.com>
2025-03-14 17:09:10 +0700

    fix: Increase load test parameters and update API endpoint for testing

--- deployment/script/load-test.js
+++ deployment/script/load-test.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 2000,           // Virtual Users (concurrent users)
-  duration: '300s',   // Test duration
+  vus: 50000,           // Virtual Users (concurrent users)
+  duration: '30s',   // Test duration
 };
 
 export default function () {
-  http.get('http://fastapi.localhost:8080/images');
-  sleep(0.5);
+  http.get('http://api.localhost:8080/images');
+  sleep(1);
 }
