2464d1c0a0577acb925c6538a4923d8f7b9f8baa
Thepphithak <95979163+E3tow1C@users.noreply.github.com>
2025-03-16 02:29:03 +0700

    fix: Reduce virtual users in load test to 2000 and decrease sleep duration to 0.5 seconds

--- deployment/script/load-test.js
+++ deployment/script/load-test.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 50000,           // Virtual Users (concurrent users)
+  vus: 2000,           // Virtual Users (concurrent users)
   duration: '300s',   // Test duration
 };
 
 export default function () {
   http.get('http://localhost:8080/');
-  sleep(1);
+  sleep(0.5);
 }
